1fc0f3492ee9ad52eb1acd084b933605afcc887a
Nils P. Kujath <63961429+NilsKujath@users.noreply.github.com>
2022-07-19 18:20:31 +0200

    Added setup of custom .zshrc in ~

--- devenv_setup.sh
+++ devenv_setup.sh
@@ -1,1 +1,15 @@
 #!/bin/zsh
+
+if [ -f ~/.zshrc ]
+then
+    rm ~/.zshrc
+    echo "... Removed old .zshrc file from ~."
+fi
+
+if [ ! -f ~/.zshrc ]
+then
+    cp ./.zshrc ~/
+    echo "... Added your custom .zshrc file to ~."
+fi
+
+echo "... Your development environment is set up!"
