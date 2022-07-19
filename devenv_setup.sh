40e21b179fe6466acf8f5131d1beb15b3922053a
Nils P. Kujath <63961429+NilsKujath@users.noreply.github.com>
2022-07-19 20:22:15 +0200

    Added setup for custom .gitconfig; Added installation of Homebrew

--- devenv_setup.sh
+++ devenv_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+# REPLACE LOCAL .zshrc FILE WITH CUSTOM .zshrc FILE
 if [ -f ~/.zshrc ]
 then
     rm ~/.zshrc
@@ -12,4 +13,31 @@ then
     echo "... Added your custom .zshrc file to ~."
 fi
 
+
+# REPLACE LOCAL .gitconfig FILE WITH CUSTOM .gitconfig FILE
+if [ -f ~/.gitconfig ]
+then
+    rm ~/.gitconfig
+    echo "... Removed old .gitconfig file from ~."
+fi
+
+if [ ! -f ~/.gitconfig ]
+then
+    cp ./.gitconfig ~/
+    echo "... Added your custom .gitconfig file to ~."
+fi
+
+
+# CHECK IF homebrew IS INSTALLED, IF NOT, INSTALL IT
+if ! command -v brew &> /dev/null
+then
+    rm -r /usr/local/*
+    echo "Homebrew is not installed. Initiating installation:"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+
+
+
+# INDICATE THAT SETUP IS COMPLETE
 echo "... Your development environment is set up!"
