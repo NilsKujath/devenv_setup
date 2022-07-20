45689f1cef6b726edbd5d4e6499089b0f9b09148
Nils P. Kujath <63961429+NilsKujath@users.noreply.github.com>
2022-07-20 18:42:23 +0200

    Updated devenv_setup.sh; Updated README.md

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # devenv_setup
 
+## About
+
 Automates setup of custom development environment on a new machine.
 The automated setup is done by running a zsh shell script.
 This shell script is geared towards macOS and tested on macOS Monterey 12.4.
@@ -11,14 +13,3 @@ Run `git clone https://github.com/NilsKujath/devenv_setup.git` then `cd` into `d
 
 If you dont have a preinstalled version of Git on your machine (check by running `git --version`),
 you might need to first install the xcode commandline tools (run `xcode-select --install`, afterwards run `git --version` to recheck).
-
-## ToDo
-
-* include .vimrc and init.nvim in setup
-* automatically run :PlugInstall and :PlugUpgrade on reloaded .vimrc and .init.nvim (that might need to execute a separate file)
-* brew git and reload shell (so the version switches from Apple-Git to the homebrew git, if still necessary)
-* downlad all necessary compilers for C++ (brew)
-* automatically compare versions of programs installed by brew with version list that should be installed (to make sure there are no empty dirs); automatically install what is missing
-* brew install gh for github command line tools after git has been installed
-* make sure all brew programs are up to date
-* install firefox and add personal bookmarks (and hide those bookmarks from GitHub)
--- devenv_setup.sh
+++ devenv_setup.sh
@@ -2,44 +2,46 @@
 echo ""
 
 # ===== CONFIG FILES =====
-echo "Adding your custum config files and overwriting existing ones (if necessary):"
-echo ""
-cp ./dotfiles/.zshrc ~/ && echo "Added config file for zsh shell."
-cp ./dotfiles/.gitconfig ~/ && echo "Added config file for global Git settings."
-cp ./dotfiles/.vimrc ~/ && echo "Added config file for vim."
-cp ./dotfiles/init.vim ~/.config/nvim/ && echo "Added config file for neovim."
-echo ""
-echo "All your custom config files have been added!" && echo ""
+echo "Adding your custum config files and overwriting existing ones:" && echo ""
+cp ./dotfiles/.zshrc ~/ && echo "...added config file for zsh shell."
+cp ./dotfiles/.gitconfig ~/ && echo "...added config file for global Git settings."
+cp ./dotfiles/.vimrc ~/ && echo "...added config file for vim."
+cp ./dotfiles/init.vim ~/.config/nvim/ && echo "...added config file for neovim."
+echo "" && echo "All your custom config files have been added!" && echo ""
 
 # ===== PACKAGE MANAGERS =====
 echo "Checking for Homebrew:"
 if ! command -v brew &> /dev/null
 then
     rm -r /usr/local/*
-    echo "Homebrew is not installed. Initiating installation:"
+    echo "...Homebrew is not installed. Initiating installation:"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "Successfully Installed Hombrew" & echo ""
 else
-    echo "Updating Homebrew:"
+    echo "...updating Homebrew:"
     brew update
     brew upgrade
     if [ ! -d /usr/local/Cellar/neovim ]; then brew install neovim; fi
-    if [ ! -d /usr/local/Cellar/node ]; then brew install node; fi #dependency of coc.vim plugin
+    if [ ! -d /usr/local/Cellar/node ]; then brew install node; fi  #dependency of coc.vim plugin
+    if [ ! -d /usr/local/Cellar/git ]; then brew install git; fi
+    if [ ! -d /usr/local/Cellar/gh ]; then brew install gh; fi      #GitHub command-line tool
     echo ""
 fi
 
 echo "Checking for vim-plug:"
 if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
-    echo "Installing vim-plug for neovim:"
+    echo "...installing vim-plug for neovim:"
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
-    echo "Installing vim-plug for vim:"
+    echo "...installing vim-plug for vim:"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 echo "vim-plug is installed for vim and neovim!" && echo ""
 
 # ===== END =====
-echo "Your development environment is set up!"
+echo "Your development environment is set up!" && echo ""
+echo "What you need to do next:"
+echo "=> open vim or nvim and run ':PlugInstall' and ':PlugUpgrade'" && echo ""
