8f67065fa806a249e96071509bb5acaf1de43e6f
Nils P. Kujath <63961429+NilsKujath@users.noreply.github.com>
2022-07-20 15:25:42 +0200

    Simplified devenv_setup.sh by removing unnecessary checking; Included addition of .vimrc and init.nvim

--- devenv_setup.sh
+++ devenv_setup.sh
@@ -1,42 +1,45 @@
 #!/bin/zsh
+echo ""
 
-# REPLACE LOCAL .zshrc FILE WITH CUSTOM .zshrc FILE
-if [ -f ~/.zshrc ]; then rm ~/.zshrc && echo "Removed old .zshrc file from ~."; fi
-if [ ! -f ~/.zshrc ]; then cp ./.zshrc ~/ && echo "Added your custom .zshrc file to ~."; fi
+# ===== CONFIG FILES =====
+echo "Adding your custum config files and overwriting existing ones (if necessary):"
+echo ""
+cp ./.zshrc ~/ && echo "Added config file for zsh shell."
+cp ./.gitconfig ~/ && echo "Added config file for global Git settings."
+cp ./.vimrc ~/ && echo "Added config file for vim."
+cp ./init.vim ~/.config/nvim/ && echo "Added config file for neovim."
+echo ""
+echo "All your custom config files have been added!" && echo ""
 
-# REPLACE LOCAL .gitconfig FILE WITH CUSTOM .gitconfig FILE
-if [ -f ~/.gitconfig ]; then rm ~/.gitconfig && echo "Removed old .gitconfig file from ~."; fi
-if [ ! -f ~/.gitconfig ]; then cp ./.gitconfig ~/ && echo "Added your custom .gitconfig file to ~."; fi
-
-# CHECK IF homebrew IS INSTALLED, IF NOT, INSTALL IT
+# ===== PACKAGE MANAGERS =====
+echo "Checking for Homebrew:"
 if ! command -v brew &> /dev/null
 then
     rm -r /usr/local/*
     echo "Homebrew is not installed. Initiating installation:"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "Successfully Installed Hombrew"
+    echo "Successfully Installed Hombrew" & echo ""
 else
-    echo "Updating Homebrew"
+    echo "Updating Homebrew:"
     brew update
     brew upgrade
     if [ ! -d /usr/local/Cellar/neovim ]; then brew install neovim; fi
     if [ ! -d /usr/local/Cellar/node ]; then brew install node; fi #dependency of coc.vim plugin
+    echo ""
 fi
 
-# LOAD vim-plug PLUGIN MANAGER FOR NEOVIM
+echo "Checking for vim-plug:"
 if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
     echo "Installing vim-plug for neovim:"
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
-
-# LOAD vim-plug PLUGIN MANAGER FOR VIM
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
     echo "Installing vim-plug for vim:"
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+echo "vim-plug is installed for vim and neovim!" && echo ""
 
-
-# INDICATE THAT SETUP IS COMPLETE
+# ===== END =====
 echo "Your development environment is set up!"
