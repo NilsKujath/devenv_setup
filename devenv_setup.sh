b2b0a6976b30b2477265fd08b8a1e9ccef4379a2
Nils P. Kujath <63961429+NilsKujath@users.noreply.github.com>
2022-08-21 12:28:32 +0200

    Added license statements to all files

--- devenv_setup.sh
+++ devenv_setup.sh
@@ -1,15 +1,45 @@
+# BSD 3-Clause License
+
+# Copyright (c) 2022, Nils P. Kujath
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #!/bin/zsh
 echo ""
 
 echo "=============================================================="
 echo "Adding your custum config files and overwriting existing ones:"
 echo "==============================================================" && echo ""
-cp ./dotfiles/.zshrc ~/ && echo "...added config file for zsh shell."
-cp ./dotfiles/.gitconfig ~/ && echo "...added config file for global Git settings."
-cp ./dotfiles/.vimrc ~/ && echo "...added config file for vim."
-cp ./dotfiles/init.vim ~/.config/nvim/ && echo "...added config file for neovim."
-cp ./dotfiles/.gitignore_global ~/ && echo "...added global gitignore settings." 
-cp ./dotfiles/texstudio.ini ~/.config/texstudio/ && echo "...added config file for texstudio."
+cp ./dotfiles/.zshrc ~/ && echo "...added config file for zsh shell"
+cp ./dotfiles/.gitconfig ~/ && echo "...added config file for global Git settings"
+cp ./dotfiles/.vimrc ~/ && echo "...added config file for vim"
+cp ./dotfiles/init.vim ~/.config/nvim/ && echo "...added config file for neovim"
+cp ./dotfiles/.gitignore_global ~/ && echo "...added global gitignore settings" 
+cp ./dotfiles/texstudio.ini ~/.config/texstudio/ && echo "...added config file for texstudio"
 echo "All your custom config files have been added!" && echo ""
 
 echo "=============================================================="
@@ -17,12 +47,12 @@ echo "Checking Homebrew (package manager) and managing packages:"
 echo "==============================================================" && echo ""
 if ! command -v brew &> /dev/null; then
     rm -r /usr/local/*
-    echo "...Homebrew (package manager) is not installed. Initiating installation:"
+    echo "...Homebrew (package manager) is not installed; initiating installation:"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "Successfully installed Homebrew (package manager)." & echo ""
 else
-    echo "...updating Homebrew (package manager)." && brew update
-    echo "...checking for missing packages." && brew upgrade
+    echo "...updating Homebrew (package manager)" && brew update
+    echo "...checking for missing packages" && brew upgrade
     if [ ! -d /usr/local/Cellar/neovim ]; then brew install neovim; fi
     if [ ! -d /usr/local/Cellar/node ]; then brew install node; fi  #dependency of coc.vim plugin
     if [ ! -d /usr/local/Cellar/git ]; then brew install git; fi
@@ -39,7 +69,7 @@ else
         mysql.server start  # required to set password for mysql server
         mysql_secure_installation
         mysql.server stop; fi
-    brew cleanup && echo "...removing unused and uninstalled files."
+    brew cleanup && echo "...removing unused and uninstalled files"
     echo "All necessary packages are installed!" && echo ""
 fi
 
